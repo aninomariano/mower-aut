ffe6c14aa9d49a51a88240ce66f0b5d0ab306772
Mariano Añino <marianoanino@MacBook-Pro-de-Mariano.local>
2022-10-29 19:09:34 +0200

    init

---
+++ src/test/java/com/seatcode/moweraut/MowerAutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seatcode.moweraut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MowerAutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
